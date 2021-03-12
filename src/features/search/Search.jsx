cc85214396f9149e1340201715bfe00d7fea16e1
cagadas12 <cagadascatherine09@gmail.com>
2021-03-12 13:00:58 -0800

    CAGADAS_CATHERINE:Initial setup for store-locatot

--- src/features/search/Search.jsx
+++ src/features/search/Search.jsx
@@ -6,22 +6,27 @@ import './search.scss';
 const { Option } =Select;
 
 class Search extends Component {  
+   onFormSubmit(event) {
+      event.preventDefault();
+   }
     render() {     
       return (
          <div className='search-component'>
-            <form className='search-component-form'>
+            <form className='search-component-form' onSubmit={(event) => this.onFormSubmit(event)}>
                <div>
                   <Input 
                       name='query'
                       className='search'
                       size='large'
-                       placeholder='Search for (e.g Hardware)'                                          
+                       placeholder='Search for (e.g Hardware)' 
+                       value={this.props.query}   
+                       onChange ={(event) => this.props.onChange(event)}                                    
                     />
               </div>
               <div>
                  <div className='search-option'>
                     <Input.Group compact>
-                       <Input name='distance' />                                          
+                       <Input name='distance' value={this.props.distance} />                                          
                         <Select defaultValue='Distance'>
                            <Option value='Distance'>Distance (km)</Option>
                            <Option value='Current location'>Current location</Option>
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,6 +12,8 @@ const { Content } = Layout;
 class Main extends Component {
   state = {
     currentPosition: null,
+    query: '',
+    distance: '1',
   };
 
   componentDidMount() {
@@ -23,14 +25,18 @@ class Main extends Component {
       this.setState({ currentPosition });
     });
   }
-
+  
+  onInputChange(event) {
+    const stateKey = event.target.name;
+    this.setState({ [stateKey]: event.target.value});
+  }
 
   render() {
     return (
       <div className='main-layout'>
         <content className='content'>
           <Brand />
-          <Search />
+          <Search query={this.state.query} distance={this.state.distance}  onChange={(event) => this.onInputChange(event)}/>
           <div className='search-content'>
             <Map currentPosition={this.state.currentPosition} />
             <SearchResult />
